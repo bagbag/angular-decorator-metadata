ef0b4e837e47051dc04d63088f961f82bbbcc765
Patrick Hein <patrick-hein@outlook.com>
2024-08-04 13:19:33 +0200

    add design type test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,31 @@
+import 'reflect-metadata/lite';
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+function Property(): PropertyDecorator {
+  return function (_target: Object, _propertyKey: string | symbol): void { /* noop */ }
+}
+
+class Model {
+  @Property()
+  name!: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+  @if (designType) {
+    <div>Design type: {{ designType.name }}</div>
+  }
+  @else {
+    <div>Could not get design type</div>
+  }
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'angular-decorator-metadata';
+  readonly designType = Reflect.getMetadata('design:type', Model.prototype, 'name');
 }
